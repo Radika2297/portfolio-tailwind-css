ec4a0f346ac262050c1431786d79d18ffda2cac2
Radika2297 <radika2906@gmail.com>
2024-01-18 22:32:05 +0700

    menambahkan fitur dark mode + perbaikan fitur lainnya

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,10 +3,17 @@
 window.onscroll = function () {
   const header = document.querySelector('header')
   const fixedNav = header.offsetTop
+  const toTOp = document.querySelector('#to-top')
+
+
 
   if(window.scrollY > fixedNav) {
     header.classList.add('navbar-fixed')
+    toTOp.classList.remove('hidden')
+    toTOp.classList.add('flex')
   } else{
+    toTOp.classList.add('hidden')
+    toTOp.classList.remove('flex')
     header.classList.remove('navbar-fixed')
   }
 }
@@ -21,3 +28,37 @@ hamburger.addEventListener('click', function(){
   hamburger.classList.toggle('hamburger-active')
   navMenu.classList.toggle('hidden')
 })
+
+
+// klik di luar hamburger
+
+window.addEventListener('click', function(e){
+  if ( e.target != hamburger && e.target != navMenu){
+    hamburger.classList.remove('hamburger-active')
+    navMenu.classList.add('hidden')
+  }
+})
+
+
+// Darkmode Toggle
+
+const darkToggle = document.querySelector('#dark-toggle')
+const html = document.querySelector('html')
+
+darkToggle.addEventListener('click', function (){
+  if(darkToggle.checked) {
+    html.classList.add('dark')
+    localStorage.theme = 'dark'
+  } else{
+    html.classList.remove('dark')
+    localStorage.theme = 'light'
+  }
+})
+
+
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  darkToggle.checked = true
+} else {
+  darkToggle.checked = false
+}
+
